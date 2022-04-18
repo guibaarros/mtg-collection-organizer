0b417a4012b3cce7aa572d69f280f9502d33a66a
guibaarros <guilhermehr00@gmail.com>
2022-04-18 04:54:37 -0300

    feat: add basic app structure
    issue: #2

---
+++ src/main/java/com/mtgcollectionorganizer/api/MtgCollectionOrganizerApiModuleApplication.java
@@ -0,0 +1,13 @@
+package com.mtgcollectionorganizer.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgCollectionOrganizerApiModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgCollectionOrganizerApiModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtgcollectionorganizer/api/MtgCollectionOrganizerApiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtgcollectionorganizer.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgCollectionOrganizerApiModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
