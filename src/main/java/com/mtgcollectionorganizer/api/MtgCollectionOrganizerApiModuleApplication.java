24a84eaa5b15c9eda6f9f584eb5c3b1300394f53
Guilherme Barros <guilhermehr00@gmail.com>
2022-10-22 04:39:01 -0300

    feat: configure JPA Repositories, Swagger and FeignClient

--- src/main/java/com/mtgcollectionorganizer/api/MtgCollectionOrganizerApiModuleApplication.java
+++ src/main/java/com/mtgcollectionorganizer/api/MtgCollectionOrganizerApiModuleApplication.java
@@ -2,8 +2,14 @@ package com.mtgcollectionorganizer.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.mtgcollectionorganizer.api")
+@EnableSwagger2
+@EnableFeignClients
 public class MtgCollectionOrganizerApiModuleApplication {
 
 	public static void main(String[] args) {
